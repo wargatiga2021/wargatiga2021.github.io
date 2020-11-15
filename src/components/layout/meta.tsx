8f5acb7774cb711789825b7bd95dceebc2ca4361
Farell Faiz <farellfaiz@gmail.com>
2020-11-16 00:06:57 +0700

    Update metadata.

--- src/components/layout/meta.tsx
+++ src/components/layout/meta.tsx
@@ -3,17 +3,17 @@ import Head from "next/head";
 const Meta = () => {
   return (
     <Head>
-      <title>Hello | sozonome</title>
+      <title>Hello | Warga Tiga 2021</title>
       <link rel="icon" href="/favicon.ico" />
       <meta
         name="description"
-        content="Agustinus Nathaniel's Dev Site | sozonome"
+        content="Warga Tiga 2021's Website"
       />
       <link
         rel="alternate"
         type="application/rss+xml"
-        title="RSS feed for sozonome's blog posts"
-        href="https://sznm.dev/rss.xml"
+        title="RSS feed for Warga Tiga 2021 blog posts"
+        href="https://2021.wargati.ga/rss.xml"
       />
     </Head>
   );
