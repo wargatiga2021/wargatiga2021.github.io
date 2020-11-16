2c49c20abfd39d2fa82f441f0242f3a8526506c6
Farell Faiz <farellfaiz@gmail.com>
2020-11-16 18:27:05 +0700

    Update turbud page.

--- src/pages/turbud.tsx
+++ src/pages/turbud.tsx
@@ -1,51 +1,49 @@
-import { Heading, Text, Link, Box, BoxProps } from "@chakra-ui/react";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { Box, Heading, Text, Link, Button } from "@chakra-ui/react";
+import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import MotionFlex from "../../components/motion/MotionFlex";
+import { useEffect } from "react";
 import Head from "next/head";
-
-import AccessibleLink from "../components/AccessibleLink";
-import MotionBox from "../components/motion/MotionBox";
-import MotionFlex from "../components/motion/MotionFlex"; 
 
 const Turbud = () => {
   return (
-    <Box>
+    <>
       <Head>
         <title>Turbud | Warga Tiga 2021</title>
       </Head>
-      <HeadSection />
-      <AboutContent />
-    </Box>
-  );
-};
-
-const HeadSection = () => {
-  return (
-    <Box marginBottom={8}>
-      <Heading>🏖 Tur Budaya</Heading>
-    </Box>
-  );
-};
-
-const AboutContent = () => {
-  const textProps: BoxProps = {
-    marginY: 4,
-  };
-
-  return (
-    <MotionBox
-      variants={{
-        before: { opacity: 0, y: 20, transition: { type: "spring" } },
-        after: { opacity: 1, y: 0, transition: { type: "spring" } },
-      }}
-      initial="before"
-      animate="after"
-    >
-      <Text {...textProps}>
-          Halaman dalam pengembangan.
-      </Text>
-    </MotionBox>
+      <MotionFlex
+        marginY={"2.75rem"}
+        as="section"
+        variants={{
+          before: { opacity: 0, y: 20, transition: { type: "spring" } },
+          after: { opacity: 1, y: 0, transition: { type: "spring" } },
+        }}
+        initial="before"
+        animate="after"
+      >
+        <Box boxSize={[100, 160]}>
+          <Image
+            width={160}
+            height={160}
+            src="/app_icons/icon_advicegen.png"
+            alt="Warga Tiga 2021"
+          />
+        </Box>
+        <Box
+          verticalAlign="center"
+          width={["100%", "80%", "60%"]}
+          paddingX={22}
+          alignSelf={["none", "center"]}
+        >
+          <Heading as="h1" size="2xl" paddingBottom={11}>
+            Tur Budaya 3'2021
+          </Heading>
+          <Text fontSize="xl">
+            Halaman ini sedang dikembangkan.
+          </Text>
+        </Box>
+      </MotionFlex>
+    </>
   );
 };
 
