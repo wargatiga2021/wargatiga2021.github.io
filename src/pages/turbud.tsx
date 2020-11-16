387d1b6905f71f8f94a087ec7192f61d51c23852
Farell Faiz <farellfaiz@gmail.com>
2020-11-16 18:30:47 +0700

    Fix module error.

--- src/pages/turbud.tsx
+++ src/pages/turbud.tsx
@@ -1,7 +1,7 @@
 import { Box, Heading, Text, Link, Button } from "@chakra-ui/react";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import MotionFlex from "../../components/motion/MotionFlex";
+import MotionFlex from "../components/motion/MotionFlex";
 import { useEffect } from "react";
 import Head from "next/head";
 
