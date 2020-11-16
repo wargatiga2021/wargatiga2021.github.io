f29385ddcee77e43d3f152524afcad9134d5b22a
Farell Faiz <farellfaiz@gmail.com>
2020-11-16 13:36:38 +0700

    Add booklet and turbud page.

---
+++ src/pages/turbud.tsx
@@ -0,0 +1,53 @@
+import { Heading, Text, Link, Box, BoxProps } from "@chakra-ui/react";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Head from "next/head";
+
+import AccessibleLink from "../components/AccessibleLink";
+import MotionBox from "../components/motion/MotionBox";
+import MotionFlex from "../components/motion/MotionFlex";
+
+const About = () => {
+  return (
+    <Box>
+      <Head>
+        <title>About | Warga Tiga 2021</title>
+      </Head>
+      <HeadSection />
+      <AboutContent />
+      <Links />
+    </Box>
+  );
+};
+
+const HeadSection = () => {
+  return (
+    <Box marginBottom={8}>
+      <Heading>🏖 Tur Budaya</Heading>
+    </Box>
+  );
+};
+
+const AboutContent = () => {
+  const textProps: BoxProps = {
+    marginY: 4,
+  };
+
+  return (
+    <MotionBox
+      variants={{
+        before: { opacity: 0, y: 20, transition: { type: "spring" } },
+        after: { opacity: 1, y: 0, transition: { type: "spring" } },
+      }}
+      initial="before"
+      animate="after"
+    >
+      <Text {...textProps}>
+          Halaman dalam pengembangan.
+      </Text>
+    </MotionBox>
+  );
+};
+
+export default About;
