f172e19e53496e11548c51c1e8e231e94b5ab746
Farell Faiz <farellfaiz@gmail.com>
2020-11-16 15:44:47 +0700

    Add animation and layout in booklet page.

--- src/pages/booklet/index.tsx
+++ src/pages/booklet/index.tsx
@@ -5,18 +5,40 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Booklet = () => {
   return (
     <>
-      <Box as="article">
+    <MotionFlex
+      marginY={"2.75rem"}
+      as="section"
+      variants={{
+        before: { opacity: 0, y: 20, transition: { type: "spring" } },
+        after: { opacity: 1, y: 0, transition: { type: "spring" } },
+      }}
+      initial="before"
+      animate="after"
+    >
+      <Box boxSize={[100, 160]}>
         <Image
+          width={160}
+          height={160}
           src="/app_icons/desbook-pwa.png"
-          width={110}
-          height={110}
-          alt="Booklet"
+          alt="Warga Tiga 2021"
         />
-        <Box marginY={22}>
-          <Heading as="h1" size="2xl">
-            Booklet 3'2021
-          </Heading>
-        </Box>
+      </Box>
+      <Box
+        verticalAlign="center"
+        width={["100%", "80%", "60%"]}
+        paddingX={22}
+        alignSelf={["none", "center"]}
+      >
+        <Heading as="h1" size="2xl" paddingBottom={11}>
+          Booklet 3'2021
+        </Heading>
+        <Text fontSize="xl">
+          Di halaman ini kalian dapat mengakses informasi terkait booklet 3'2021.
+           Tahun ini, SMAN 3 Bandung bekerja sama dengan Nawa Factory dalam pembuatan 
+           booklet.
+        </Text>
+      </Box>
+    </MotionFlex>
         <Box marginY={22}>
           <Heading as="h2" size="lg">
               Timeline Booklet
