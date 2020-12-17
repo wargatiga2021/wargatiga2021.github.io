aac338f9c4bace5343beb9ecbb93804be6fdf003
Farell Faiz <farellfaiz@gmail.com>
2020-12-17 17:38:30 +0700

    Add map and set align value of Flex to center.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,25 +1,23 @@
 import Link from "next/link";
-import { Box, Flex, Text, Image, Center, Heading, Badge, useColorModeValue, useColorMode } from "@chakra-ui/react";
+import { Box, Flex, Text, Image, HStack, Center, Heading, Badge, useColorModeValue, useColorMode, IconButton, useDisclosure } from "@chakra-ui/react";
 import Logo from "./Logo";
 import ThemeToggle from "./ThemeToggle";
+import Map from "./Map";
 
 import { responsiveWidthProps } from "../../styles/customTheme";
 
 const Header = () => {
   return (
-    <Flex as="header" {...responsiveWidthProps}>
-      <Center>
+    <Flex as="header" {...responsiveWidthProps} align="center">
       <Logo />
-      </Center>
       <Link href="/" passHref>
         <Heading as="a" cursor="pointer" fontSize="4xl" fontFamily="Markazi Text, sans-serif">
           Warga Tiga
         </Heading>
-      </Link><br/>
-      <Center>
-        <Badge ml="2" fontSize="1em" variant="subtle">2021</Badge>
-      </Center>
+      </Link>
+      <Badge ml="2" fontSize="1em" variant="subtle">2021</Badge>
       <Box marginLeft="auto">
+        <Map />
         <ThemeToggle />
       </Box>
     </Flex>
