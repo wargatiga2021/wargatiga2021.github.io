cba0a224eb4b839b5cb9a4e8c84c3095a3e52134
Farell Faiz <farellfaiz@gmail.com>
2020-12-17 17:37:47 +0700

    Update ThemeToggle.

--- src/components/layout/ThemeToggle.tsx
+++ src/components/layout/ThemeToggle.tsx
@@ -1,19 +1,28 @@
-import { Box, IconButton, useColorMode } from "@chakra-ui/react";
+import { Box, IconButton, useColorMode, useDisclosure } from "@chakra-ui/react";
 import { BsMoon } from "react-icons/bs";
 import { FiSun } from "react-icons/fi";
+import { FaMapMarkedAlt } from "react-icons/fa"
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button
+} from "@chakra-ui/react"
 
 const ThemeToggle = () => {
   const { colorMode, toggleColorMode } = useColorMode();
-
+        
   return (
-    <Box>
-      <IconButton
-        aria-label="toggle theme mode"
-        icon={colorMode === "light" ? <BsMoon /> : <FiSun />}
-        onClick={toggleColorMode}
-        backgroundColor={colorMode === "light" ? "gray.400" : "teal.900"}
-      />
-    </Box>
+    <IconButton
+      aria-label="toggle theme mode"
+      icon={colorMode === "light" ? <BsMoon /> : <FiSun />}
+      onClick={toggleColorMode}
+      backgroundColor={colorMode === "light" ? "gray" : "teal.900"}
+    />
   );
 };
 
